9f9aaccf81be9a2a74fc52cbbcd7d7a1b209ea79
Omar Mohumed <wadaniinarais1@gmail.com>
2025-01-10 13:54:17 -0800

    task done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-# Install an especific version of flask (2.1.0)
+# Install flask version 2.1.0 using pip3
+package { 'python3-pip':
+  ensure => 'installed',
+}
 
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip',
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin', '/bin'],
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+  require => Package['python3-pip'],
 }
