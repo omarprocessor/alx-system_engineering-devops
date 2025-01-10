e3d06b5b1d10ee26f4c6633cb73d1440652ec8db
Omar Mohumed <wadaniinarais1@gmail.com>
2025-01-10 13:30:26 -0800

    task done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-#!/usr/bin/pup
 # This Puppet manifest installs Flask version 2.1.0 using pip3.
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+  require => Package['python3-pip'],
 }
