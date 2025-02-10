449d6df5d4843943ad93946ea7d5443051ac31ff
Omar Mohumed <wadaniinarais1@gmail.com>
2025-02-10 12:30:24 -0800

    task done

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.exit(1)
+
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com"
+
+    user = requests.get(f"{url}/users/{user_id}").json()
+    todos = requests.get(f"{url}/todos", params={"userId": user_id}).json()
+
+    if not user:
+        sys.exit(1)
+
+    employee_name = user.get("name")
+    done_tasks = [task.get("title") for task in todos if task.get("completed")]
+    total_tasks = len(todos)
+
+    print(f"Employee {employee_name} is done with tasks({len(done_tasks)}/{total_tasks}):")
+    for task in done_tasks:
+        print(f"\t {task}")
